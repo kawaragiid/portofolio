1499fa7d863c3a163ecebf5c1c7c308a4d88b613
kawaragiid <kowachankeigo@gmail.com>
2026-06-18 18:00:46 +0700

    banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     siteName: "Ilmi Portfolio",
     images: [
       {
-        url: "https://kawaragi.id/og-banner-main.png", // Kita akan siapkan gambarnya setelah ini
+        url: "https://keigo.kawaragi.id/og-banner-main.png", // Kita akan siapkan gambarnya setelah ini
         width: 1200,
         height: 630,
         alt: "Ilmi Portfolio Preview",
